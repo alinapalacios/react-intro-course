ec3f0580e0b665434e8099112d1198c157b9052e
Alina Palacios Begué <alinapalacios@MacBook-Air-de-Alina.local>
2026-09-12 17:46:41 -0500

    como utlizar las props para comunicar compentes parte 2

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import logo from './platzi.webp';
+
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  {text:'Cortar cebolla', complete:true},
+  {text:'Tomar el curso de intro a react', completed:false},
+  {text:'LLorar con la llorona', completed:false},
+  {text:'entender el cusso', completed:false},
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <TodoCounter completed={16} total={25}/>
       <TodoSearch/>
 
       <TodoList>
-         <TodoItem />
-         <TodoItem />
-         <TodoItem />
+        {defaultTodos.map(todo =>(
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
       </TodoList>
-
+ 
      <CreateTodoButton/> 
      
-    </div>
+    </React.Fragment>
   );
 }
 
